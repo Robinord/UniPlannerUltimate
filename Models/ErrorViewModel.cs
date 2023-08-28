f360a1dd6530119c00c61acd2c4219da91305551
ac117413 <ac117413@8CG8163KZY>
2023-08-28 14:56:11 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
