313db37fc657e60b3ef1ad3117453fe5a1928235
Saaid Faraz <ac117413@avcol.school.nz>
2023-08-29 13:30:53 +1200

    Created Programmes model.

---
+++ Models/Programme.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UniPlanner.Models;
+
+
+namespace UniPlanner.Models
+{
+    public class Programme
+    {
+        public int ProgrammeID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<UniProgramme> UniProgrammes { get; set; }
+    }
+}
