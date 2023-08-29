a20b5ed4dc9afd2d83748950064cffedc87fb41b
Saaid Faraz <ac117413@avcol.school.nz>
2023-08-29 13:31:08 +1200

    Created UniProgrammes model.

---
+++ Models/UniProgramme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPlanner.Models
+{
+    public class UniProgramme
+    {
+        public int UniProgrammeID { get; set; }
+        public int UniversityInfoID { get; set; }
+        public  int ProgrammeID { get; set; }
+        public  string Link { get; set; }
+        [Range(0, 320, ErrorMessage = "Please enter correct value")]
+        public int? RankScore { get; set; }
+
+
+        
+        public UniversityInfo UniversityInfo { get; set; }
+        public Programme Programme { get; set; } 
+        public  ICollection<MajorsOffered> Majors { get; set; }
+    }
+}
