e978fdaf9c1bf1be8cf15faceab1abfbef739a07
Saaid Faraz <ac117413@avcol.school.nz>
2023-08-29 13:29:58 +1200

    Created UniversityInfo model.

---
+++ Models/UniversityInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPlanner.Models
+{
+    public class UniversityInfo
+    {
+        public int UniversityInfoID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public int THErank { get; set; }
+        public int QSrank { get; set; }
+        public int ARWUrank { get; set; }
+
+        public ICollection<UniProgramme> UniProgrammes { get; set; }
+    }
+}
