b3b15a4fb3ae54cecbb60d753f5ddfd3295166ba
Saaid Faraz <ac117413@avcol.school.nz>
2023-08-29 13:31:26 +1200

    Created MajorsOffered model.

---
+++ Models/MajorsOffered.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPlanner.Models
+{
+    public class MajorsOffered
+    {
+        [Key]
+        public int MajorsOfferedID { get; set; }
+        public int UniProgrammeID { get; set; }   
+        public  string Name { get; set; }
+        public string Link { get; set; }
+        public UniProgramme UniProgramme { get; set; }
+    }
+}
