4119e1bfb927cb67b6efb59850aff324f668d699
Saaid Faraz <ac117413@avcol.school.nz>
2023-09-18 15:12:53 +1200

    Added validation for ranks.

--- Models/UniversityInfo.cs
+++ Models/UniversityInfo.cs
@@ -9,11 +9,15 @@ namespace UniPlanner.Models
         public string City { get; set; }
         public string Region { get; set; }
         // Display added for ease of use
+        // validation for rank
         [Display(Name ="THE Rank")]
+        [Range(1, int.MaxValue,  ErrorMessage =" Enter Valid rank" )] //validation for rank
         public int THErank { get; set; }
         [Display(Name = "QS Rank")]
+        [Range(1, int.MaxValue, ErrorMessage = " Enter Valid rank")]
         public int QSrank { get; set; }
         [Display(Name = "ARWU Rank")]
+        [Range(1, int.MaxValue, ErrorMessage = " Enter Valid rank")]
         public int ARWUrank { get; set; }
 
         public ICollection<UniProgramme> UniProgrammes { get; set; }
