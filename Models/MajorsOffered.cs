756e319c4f7dd63751507dc1a3eb1df67e79b030
Saaid Faraz <farazsaaid@gmail.com>
2023-09-28 23:19:52 +1300

    Changed Diplay Name of "Name" in Majors Offered.

--- Models/MajorsOffered.cs
+++ Models/MajorsOffered.cs
@@ -10,7 +10,8 @@ namespace UniPlanner.Models
         [Display(Name = "Majors Offered ID")]
         public int MajorsOfferedID { get; set; }
         [Display(Name = "Programmes Offered ID")]
-        public int UniProgrammeID { get; set; }   
+        public int UniProgrammeID { get; set; }
+        [Display(Name = "Major Name")]
         public  string Name { get; set; }
         [Display(Name = "Major Link")]
         public string Link { get; set; }
